a5173e9282369e119390db99b3c40937926b8da0
Junior Silva <juniorjds2015@gmail.com>
2024-02-14 16:56:41 -0300

    feat: base file

---
+++ api/route/base.go
@@ -0,0 +1,14 @@
+package route
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func responseWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
