e44cb0375e8514587ee9f4b70e1257474131b7bb
Junior Silva <juniorjds2015@gmail.com>
2024-02-14 21:24:21 -0300

    refactor: query com unnest para dimiinuir o tempo de insercao

--- repository/user.go
+++ repository/user.go
@@ -1,7 +1,7 @@
 package repository
 
 import (
-	"fmt"
+	"context"
 
 	"github.com/JuniorJDS/data-handler-api/entity"
 	"github.com/JuniorJDS/data-handler-api/infra"
@@ -19,12 +19,41 @@ func NewUserRepository() *UserRepository {
 }
 
 func (u *UserRepository) InsertManyRows(data []entity.UserData) error {
-	sqlQuery := `INSERT INTO 
-		userData (cpf, private, incompleto, lastdate, avgticket, lastTicket,  storefrequent, storelast) 
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
+	query := `
+		INSERT INTO userdata
+		(cpf, private, incompleto, datadaultimacompra, ticketmedio, ticketdaultimacompra, lojamaisfrequente, lojadaultimacompra) 
+		  (
+			select * from unnest($1::varchar[], $2::boolean[], $3::boolean[], $4::varchar[], $5::numeric[], $6::numeric[], $7::varchar[], $8::varchar[])
+		  )`
 
-	// u.db.Exec()
-	fmt.Println(sqlQuery)
+	cpfAux, privateAux, incompletoAux := []string{}, []bool{}, []bool{}
+	datadaultimacompraAux, ticketmedioAux, ticketdaultimacompraAux := []string{}, []*float64{}, []*float64{}
+	lojamaisfrequenteAux, lojadaultimacompraAux := []string{}, []string{}
+	for _, userData := range data {
+		cpfAux = append(cpfAux, userData.CPF)
+		privateAux = append(privateAux, userData.Private)
+		incompletoAux = append(incompletoAux, userData.Incompleto)
+		datadaultimacompraAux = append(datadaultimacompraAux, userData.DataDaUltimaCompra)
+		ticketmedioAux = append(ticketmedioAux, userData.TicketMedio)
+		ticketdaultimacompraAux = append(ticketdaultimacompraAux, userData.TicketDaUltimaCompra)
+		lojamaisfrequenteAux = append(lojamaisfrequenteAux, userData.LojaMaisFrequente)
+		lojadaultimacompraAux = append(lojadaultimacompraAux, userData.LojaDaUltimaCompra)
+	}
+
+	if _, err := u.db.Exec(
+		context.Background(),
+		query,
+		cpfAux,
+		privateAux,
+		incompletoAux,
+		datadaultimacompraAux,
+		ticketmedioAux,
+		ticketdaultimacompraAux,
+		lojamaisfrequenteAux,
+		lojadaultimacompraAux,
+	); err != nil {
+		return err
+	}
 
 	return nil
 }
